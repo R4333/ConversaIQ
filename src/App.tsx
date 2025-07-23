3d55d57dd129ade177e563b675da8c98453629e1
R4333 <abdullahch433@gmail.com>
2025-07-24 00:59:10 +0500

    setup nodeintegration, shadcn, tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/electron-vite.animate.svg'
 import './App.css'
+import fs from 'fs'
+
+import { Button } from './components/ui/button'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,12 +22,20 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button className="items-center gap-2 " onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
+        {/* <button className="bg-amber-50  dark:bg-amber-900 text-amber-800 dark:text-amber-200 p-4 rounded-lg shadow-md " onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button> */}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p className= "bg-amber-50 dark:bg-amber-900 text-amber-800 dark:text-amber-200 p-4 rounded-lg shadow-md">
+          files: {fs.readdirSync('./').join(', ')}
+        </p>
+        
+        
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
